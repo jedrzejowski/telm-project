158ebbe8fd40832f2a3d1724b89073fdb20aa89b
Adam Jędrzejowski <adam@jedrzejowski.pl>
2020-11-11 14:44:56 +0100

    prace nad produkcją

--- makedeb.sh
+++ makedeb.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+set -e
+
 tempdir=$(mktemp -d)
-#trap "rm -rf $tempdir" EXIT
+trap "rm -rf $tempdir" EXIT
 
-pkgdir="$tempdir"
+function dockersave() {
+    tag=$1
 
+    if [[ "$(docker images -q $tag 2> /dev/null)" == "" ]]; then
+        docker pull $tag
+    fi
 
+    docker save $tag | gzip > "$tempdir/$tag.tar.gz"
+}
+
+function makedocker() {
+    mytag="telm-project-node:$(jq .version package.json -r)"
+    #docker build --file node.dockerfile --tag $mytag .
+    docker save --output "$tempdir/$mytag.tar" $mytag
+
+    dockersave postgres:12-alpine
+    dockersave nginx:1.18-alpine
+    dockersave redis:6-alpine
+}
 
 function makedeb() {
     pkgdir=$1
+    tempdir=$2
+    pkgdir="$tempdir/pkg"
 
     pkgname=$(jq .name package.json -r)
     pkgver=$(jq .version package.json -r)
@@ -22,19 +42,41 @@ Version: $pkgver
 Section: custom
 Priority: optional
 Architecture: all
-Maintainer: noone <example@example.com>
+Maintainer: noone <noone@example.com>
 Depends: docker, docker-compose
 Description: $pkgdesc
 EOF
 
-    install -Dm755 docker-compose.yml "$pkgdir/usr/$(pkgname)"
+    appdir="/lib/$pkgname"
+    installdir="$pkgdir/$appdir"
+
+    install -dm755 "$pkgdir/bin" "$pkgdir/var/lib/$pkgname"
+
+    install -Dm644 docker-compose.prod.yml "$installdir/docker-compose.yml"
+    install -Dm644 nginx.nginx "$installdir/nginx.nginx"
+    install -Dm644 .prod.env "$pkgdir/etc/$pkgname.conf"
+
+    cat << EOF >> "$pkgdir/bin/$pkgname"
+#!/bin/bash
+
+APP_VOLUME=/var/lib/$pkgname \\
+docker-compose \\
+    --project-name telm_project \\
+    --env-file "$appdir/env" \\
+    --env-file "/etc/$pkgname.conf" \\
+    --file "$appdir/docker-compose.yml" \\
+    "\$@"
+
+EOF
+    chmod 755 "$pkgdir/bin/$pkgname"
 
 }
 
-
+makedocker
 fakeroot bash -c "
 $(LC_ALL=C type makedeb | tail -n +2)
-makedeb '$pkgdir'
+makedeb '$pkgdir' '$tempdir'
 "
 
-tree $tempdir
+tree -a $tempdir
+dpkg-deb --build "$tempdir/pkg" ./
